a74cee3dbe90e0bdde57ab8d2928b81852538c15
jensenw <wj@wade-jensen.com>
2018-02-11 18:36:28 +1000

    cleanup

--- lib/Server.ts
+++ lib/Server.ts
@@ -4,8 +4,6 @@ import * as util from "util"
 
 import {FlatmatesClient} from "./api-client/FlatmatesClient"
 
-import testAuth from "./routes/test"
-
 export class Server {
     public app: express.Application
 
@@ -53,44 +51,6 @@ export class Server {
             let str = 'hello world '
             res.send(str + c)
         });
-
-        this.app.get('/testAuth', testAuth);
-    
-        this.app.get('/testMapMarkers', async function(req, res) {
-            const reqBody = {
-                "search":
-                    {
-                        "mode":"rooms",
-                        "min_budget":100,
-                        "max_budget":2000,
-                        "top_left":"-33.878453691548835,151.16001704415282",
-                        "bottom_right":"-33.90481527152859,151.2626705475708"
-                    }
-            }
-
-
-    
-            let data = await FlatmatesClient.GetListings(reqBody)
-    
-            console.log("testing map markers")
-            console.log('Test page served.')
-            res.send(data)
-        });
-    
-    
-        // this.app.get('/testAutocomplete', async function(req, res) {
-        //     const a = 10
-        //     const b = 20
-        //     let c = a * b
-        //     let str = 'hello world '
-    
-        //     let json = await app.flatmatesClient.autocomplete("Redfern")
-    
-        //     res.send(json)
-        //     console.log('Test page served.')
-        //     console.log(json)
-        // });
-    
     }
 
     private api() {
--- lib/test.ts
+++
@@ -1,4 +0,0 @@
-export const hello = () => 'Hello world!'
-
-
-
