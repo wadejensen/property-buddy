99682d3d19a24b656ecd5817691b9287665f7485
jensenw <wj@wade-jensen.com>
2018-02-11 11:41:28 +1000

    #2 recursive listings search method

--- test/TestGeoUtils.ts
+++ test/TestGeoUtils.ts
@@ -1,57 +1,99 @@
-import {GeoUtils} from "../lib/geoutils/GeoUtils"
+import {GeoUtils, Rectangle} from "../lib/geoutils/GeoUtils" //"../lib/geoutils/GeoUtils"
 import { expect } from "chai"
 import "mocha"
 
 import { format } from "util";
 
 class TestSearchGrid {
-    private grid: [number]
-    private maxResults: number = 0
+    public static grid: [[number]]
+    private static maxResults: number = 0
     
     /**
      * Intialise a TestSearchGrid with a dummy two dimensional array
      * @gridSize The side length of the sqare search grid generated. Should be a power of 2.
      * @maxResults The maximum number of results which can be returned by the SearchGrid function on any one call
      */
-    Initialise(gridSize, maxResults): void {
-        let grid = <[number]> Array.prototype.fill(Array.prototype.fill(1, 0, gridSize))
-        this.grid = grid
-    }
-
-    async SearchGrid(x1: number, y1: number, x2: number, y2: number) {
-        let results = []
-        for (let i = x1; i <= x2; i++) {
-            for (let j = y1; j <= y2; j++) {
-                results.push(this.grid[i][j])
+    static Initialise(gridSize: number, 
+                      maxResults: number, 
+                      valueProvider: (i: number, j: number) => number): void {
+        let grid = <[any]> new Array(gridSize)
+        for ( let i=0; i<gridSize; i++) {
+            grid[i] = new Array(gridSize)
+            for ( let j=0; j<gridSize; j++) {
+                grid[i][j] = valueProvider(i,j)
             }
         }
-        if (results.length > this.maxResults) {
-            results = results.slice(0, this.maxResults-1)
+        TestSearchGrid.grid = grid
+        TestSearchGrid.maxResults = maxResults
+    }
+
+    async SearchGrid(area: Rectangle) {
+        let x1 = area.xmin
+        let y1 = area.ymax
+        let x2 = area.xmax
+        let y2 = area.ymin
+
+        let results = []
+        for (let i = x1; i < x2; i++) {
+            for (let j = y2; j < y1; j++) {
+                results.push(TestSearchGrid.grid[i][j])
+            }
+        }
+        if (results.length > TestSearchGrid.maxResults) {
+            results = results.slice(0, TestSearchGrid.maxResults)
         }
         return await <[any]> results
     }
 }
 
-describe('GeoUtils GridSearch', async function() {
+describe('GeoUtils RecurseSearch', async function() {
     this.timeout(5000)
-    it('Should get a 52 char session token and 88 char secret.', async () => {
+    it('Should find all of the values in the test map and sum them. All maps values are 1.', async () => {
       
       let mapgrid = new TestSearchGrid()
-      let gridSize = 2**16
+      let gridSize = 2**8
       let maxResults = 1000
-      mapgrid.Initialise(gridSize, maxResults)
+      let one = () => 1
 
-      let isResultShorterThan1000 = function(results: [any]): Boolean {
-        return results.length > 1000
-      }
+      TestSearchGrid.Initialise(gridSize, maxResults, one)
 
-      let results = <[number]> await GeoUtils.GridSearch(
-          0, gridSize, 0, gridSize, /* coords */
+      let resultIsLongerThan1000 = (results: [any]) => results.length >= 1000
+
+      let results = <[number]> await GeoUtils.RecurseSearch(
+          [{}],
+          new Rectangle(0, gridSize, gridSize, 0), /* coords */
           mapgrid.SearchGrid, /* performSearch */
-          isResultShorterThan1000 /* isSearchGranularEnough */)
+          resultIsLongerThan1000 /* isSearchGranularEnough */)
       
       // sum the array
       let sumOfResults = results.reduce((a, b) => a + b, 0)
       expect(sumOfResults).to.equal( gridSize * gridSize )
     })
-  })
+})
+
+describe('GeoUtils RecurseSearch', async function() {
+    this.timeout(5000)
+    it('Should find all of the values in the test map and sum them. All maps values are random.', async () => {
+      
+        let mapgrid = new TestSearchGrid()
+        let gridSize = 2**8
+        let maxResults = 1000
+        let randBetween0And100 = () => Math.random() * 100
+
+        TestSearchGrid.Initialise(gridSize, maxResults, randBetween0And100)
+        let expected = TestSearchGrid.grid.reduce((arr1, arr2) => <[number]> arr1.concat(arr2))
+                                          .reduce((elem1,elem2) => elem1 + elem2)
+
+        let resultIsLongerThan1000 = (results: [any]) => results.length >= 1000
+
+        let results = <[number]> await GeoUtils.RecurseSearch(
+            [{}],
+            new Rectangle(0, gridSize, gridSize, 0), /* coords */
+            mapgrid.SearchGrid, /* performSearch */
+            resultIsLongerThan1000 /* isSearchGranularEnough */)
+        
+        // sum the array
+        let sumOfResults = results.reduce((elem1,elem2) => elem1 + elem2)
+        expect(sumOfResults).to.be.closeTo(expected, 0.01)
+    })
+})
